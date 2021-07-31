e3a04fa17d285ebd9157c2fcf8460adf1427f9a3
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-07-31 11:30:22 +0530

    Update Map Sum Pairs.cpp

--- Map Sum Pairs.cpp
+++ Map Sum Pairs.cpp
@@ -1,3 +1,5 @@
+// Approach 1 - using map
+
 class MapSum {
 public:
     /** Initialize your data structure here. */
@@ -31,3 +33,110 @@ public:
         
     }
 };
+
+// S.C = O(n)
+// T.C = O(n)
+
+
+// approach 2 - using trie
+
+struct TrieNode
+{
+    vector<TrieNode*> children;
+    int sum;
+    bool isEnd;
+    
+    TrieNode()
+    {
+        children.resize(26, NULL);
+        sum = 0;
+        isEnd = false;
+    }
+    
+    TrieNode* getChar(char ch)
+    {
+        return children[ch - 'a'];
+    }
+    
+    void setChar(char ch, int val)
+    {
+        children[ch - 'a'] = new TrieNode();
+        children[ch - 'a']->sum = val;
+    }
+    
+    int getSum()
+    {
+        return sum;
+    }
+    
+    int getCharSum(char ch)
+    {
+        return children[ch - 'a']->sum;
+    }
+};
+
+class MapSum {
+private:
+    TrieNode* root;
+    unordered_map<string, int> mp;
+public:
+    /** Initialize your data structure here. */
+    MapSum() 
+    {
+        root = new TrieNode();
+    }
+    
+    void insert(string key, int val) 
+    {
+        TrieNode* curr = root;
+        
+        for(auto ch : key)
+        {
+            //If there is no node existing for current character, create one and add val to it
+            if(curr->getChar(ch) == NULL)
+            {
+                curr->setChar(ch, val);
+            }
+            //We have a node existing for current character
+            else
+            {
+                //Get the sum for current character
+                int currSum = curr->getCharSum(ch);
+                
+                //If current key already exists in map.
+                if(mp.find(key) != mp.end())
+                    //Decrement current key's value from sum and add new val to it
+                    curr->getChar(ch)->sum = currSum - mp[key] + val;
+                else
+                    //Add the value to current sum
+                    curr->getChar(ch)->sum = currSum + val;
+            }
+            
+            curr = curr->getChar(ch);
+        }
+        mp[key] = val;
+    }
+    
+    int sum(string prefix) 
+    {
+        TrieNode* curr = root;
+        
+        //Iterate through each character in prefix
+        for(auto ch : prefix)
+        {
+            //If we don't have a node existing for current character, return 0
+            if(curr->getChar(ch) == NULL)
+                return 0;
+            //Else move onto next node
+            else
+                curr = curr->getChar(ch);
+        }
+        //return the sum of current node
+        return curr->getSum();
+    }
+};
+
+
+
+
+
